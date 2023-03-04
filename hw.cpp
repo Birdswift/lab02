dee0edda7670ca7b61699bf7f2d1349239c476ab
Birdswift <112816261+Birdswift@users.noreply.github.com>
2023-03-04 16:01:13 +0300

    Update hw.cpp

--- hw.cpp
+++ hw.cpp
@@ -3,7 +3,7 @@
 
 void main(){
 std::string name = "";
-std::cout << "Hello, please, input your name." <<std::endl; //запрос имени
-std::cin >> name; //ввод имени
-std::cout << "Hello world from " << name <<std::endl; //вывод имени
+std::cout << "Hello, please, input your name." <<std::endl; //запрос имени?
+std::cin >> name; //ввод имени.
+std::cout << "Hello world from " << name <<std::endl; //вывод имени!
 }
