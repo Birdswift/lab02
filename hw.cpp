2ab6f60a42a08bf61b8f7c1d17257da3084674cd
Birdswift <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>
2023-03-04 15:34:46 +0300

    Formatted one more time

--- hw.cpp
+++ hw.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 void main(){
 std::string name = "";
 std::cout << "Hello, please, input your name." <<std::endl; //запрос имени?
 std::cin >> name; //ввод имени.
 std::cout << "Hello world from " << name <<std::endl; //вывод имени!
+=======
+void
+main()
+{
+  std::string name = "";
+  std::cout << "Hello, please, input your name." << std::endl; //запрос имени
+  std::cin >> name;                                      //ввод имени
+  std::cout << "Hello world from " << name << std::endl; //вывод имени
+>>>>>>> 78a0746 (Formatted)
 }
