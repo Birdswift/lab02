bb4d39b5e1090d769c348450c0fb5d84c44ca7d4
Birdswift <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>
2023-03-04 13:23:48 +0300

    New commit

--- hw.cpp
+++ hw.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 void main(){
-cout << "Hello world!" <<endl;
+string name = "";
+cout << "Hello, please, input your name." <<endl;
+cin >> name;
+cout << "Hello world from " << name << endl;
 }
